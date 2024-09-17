4e44783934d779c9a3b7c0d960dd6556a2c91abc
sleyaa <zarekparchmont14@gmail.com>
2024-09-17 16:53:36 -0600

    Add files via upload

---
+++ dowhile3.js
@@ -0,0 +1,8 @@
+//declarar variables 
+let num = 0;
+
+do{
+    num = parseInt(prompt("Ingrese un numero mayor que 100: "));
+
+}while(num<=100);
+document.write("Numero ingresado mayor que 100 es: ", num);
